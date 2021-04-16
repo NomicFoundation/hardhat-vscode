bc0dbb7a9ede13e947643a18c9ecd4fd6e7f4303
Boris Jankovic <boris@tenderly.co>
2021-04-16 19:12:58 +0200

    Implemented ContractDefinitionNode

--- parser/src/analizer/nodes/ContractDefinitionNode.ts
+++ parser/src/analizer/nodes/ContractDefinitionNode.ts
@@ -16,7 +16,19 @@ export class ContractDefinitionNode implements Node {
         this.type = contractDefinition.type;
         this.uri = uri;
         this.astNode = contractDefinition;
-        // TO-DO: Implement name location for rename
+        
+        if (contractDefinition.loc) {
+            this.nameLoc = {
+                start: {
+                    line: contractDefinition.loc.start.line,
+                    column: contractDefinition.loc.start.column + "contract ".length
+                },
+                end: {
+                    line: contractDefinition.loc.start.line,
+                    column: contractDefinition.loc.start.column + "contract ".length + contractDefinition.name.length
+                }
+            };
+        }
     }
 
     addChild(child: Node): void {
@@ -27,7 +39,13 @@ export class ContractDefinitionNode implements Node {
         this.parent = parent;
     }
 
-    accept(find: FinderType, orphanNodes: Node[], parent?: Node): void {
-        // TO-DO: Method not implemented
+    accept(find: FinderType, orphanNodes: Node[], parent: Node): void {
+        this.setParent(parent);
+
+        for (const subNode of this.astNode.subNodes) {
+            find(subNode, this.uri).accept(find, orphanNodes, this);
+        }
+
+        parent.addChild(this);
     }
 }
