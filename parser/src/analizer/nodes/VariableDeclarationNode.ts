60ee32d6c48158dc7495fcbd0cbec02d597aa9fd
Boris Jankovic <boris@tenderly.co>
2021-04-20 13:18:41 +0200

    Implemented VariableDeclarationNode

--- parser/src/analizer/nodes/VariableDeclarationNode.ts
+++ parser/src/analizer/nodes/VariableDeclarationNode.ts
@@ -15,8 +15,24 @@ export class VariableDeclarationNode implements Node {
     constructor (variableDeclaration: VariableDeclaration, uri: string) {
         this.type = variableDeclaration.type;
         this.uri = uri;
+
+        if (variableDeclaration.loc) {
+            // Bug in solidity parser doesn't give exact end location
+            variableDeclaration.loc.end.column = variableDeclaration.loc.end.column + variableDeclaration.name.length
+
+            this.nameLoc = {
+                start: {
+                    line: variableDeclaration.loc.end.line,
+                    column: variableDeclaration.loc.end.column - variableDeclaration.name.length
+                },
+                end: {
+                    line: variableDeclaration.loc.end.line,
+                    column: variableDeclaration.loc.end.column
+                }
+            };
+        }
+
         this.astNode = variableDeclaration;
-        // TO-DO: Implement name location for rename
     }
 
     addChild(child: Node): void {
@@ -28,6 +44,12 @@ export class VariableDeclarationNode implements Node {
     }
 
     accept(find: FinderType, orphanNodes: Node[], parent?: Node): void {
-        // TO-DO: Method not implemented
+        if (parent) {
+            this.setParent(parent);
+        }
+
+        find(this.astNode.typeName, this.uri).accept(find, orphanNodes, this);
+
+        parent?.addChild(this);
     }
 }
