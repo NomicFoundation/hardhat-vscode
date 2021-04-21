a6a9a5ad8fdc0e196786bc8803107dbbfdca86fc
Boris Jankovic <boris@tenderly.co>
2021-04-21 13:20:17 +0200

    Update test .sol file

--- test.sol
+++ test.sol
@@ -12,7 +12,8 @@ contract BasicToken {
 
   mapping(address => uint256) balances;
 
-  uint256 test;
+  uint256 a1 = 1;
+  uint256 test = a1;
 
   function transfer(address recipient, uint256 value) public {
     balances[msg.sender] -= value;
