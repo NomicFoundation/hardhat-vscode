676d01a59cb959b5607a206d8da879dcbd8b60b3
Boris Jankovic <boris@tenderly.co>
2021-04-21 13:20:04 +0200

    Implemented MappingNode

--- parser/src/analizer/nodes/MappingNode.ts
+++ parser/src/analizer/nodes/MappingNode.ts
@@ -16,7 +16,6 @@ export class MappingNode implements Node {
         this.type = mapping.type;
         this.uri = uri;
         this.astNode = mapping;
-        // TO-DO: Implement name location for rename
     }
 
     getName(): string | undefined {
@@ -32,6 +31,7 @@ export class MappingNode implements Node {
     }
 
     accept(find: FinderType, orphanNodes: Node[], parent?: Node): void {
-        // TO-DO: Method not implemented
+        find(this.astNode.keyType, this.uri).accept(find, orphanNodes, parent);
+        find(this.astNode.valueType, this.uri).accept(find, orphanNodes, parent);
     }
 }
