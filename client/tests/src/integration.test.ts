52c35b112e582185db8b0941b232dd5c3753891c
Boris Jankovic <boris@tenderly.co>
2021-04-28 18:28:56 +0200

    Added base for tests

---
+++ client/tests/src/integration.test.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+suite('Client integration', () => {
+	test('InitializeResult', () => {
+		// TO-DO: Impement InitializeResult
+	});
+
+	test('Goto Definition', async () => {
+		// TO-DO: Impement Definition
+	});
+
+	test('Goto Type Definition', async () => {
+		// TO-DO: Impement Type Definition
+	});
+
+	test('Find All References', async () => {
+		// TO-DO: Impement Find All References
+	});
+
+	test('Goto Implementation', async () => {
+		// TO-DO: Impement Goto Implementation
+	});
+
+	test('Do Rename', async () => {
+		// TO-DO: Impement Do Rename
+	});
+
+	test('Hover', async () => {
+		// TO-DO: Impement Hover
+	});
+});
---
+++ client/tests/src/runTests.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import * as path from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as uuid from 'uuid';
+
+import { runTests } from 'vscode-test';
+
+function rimraf(location: string) {
+	const stat = fs.lstatSync(location);
+	if (stat) {
+		if (stat.isDirectory() && !stat.isSymbolicLink()) {
+			for (const dir of fs.readdirSync(location)) {
+				rimraf(path.join(location, dir));
+			}
+
+			fs.rmdirSync(location);
+		}
+		else {
+			fs.unlinkSync(location);
+		}
+	}
+}
+
+
+async function go() {
+	try {
+		const extensionDevelopmentPath = path.resolve(__dirname, '..');
+		const extensionTestsPath = __dirname;
+
+		const testDir = path.join(os.tmpdir(), uuid.v4());
+		fs.mkdirSync(testDir, { recursive: true });
+		const userDataDir = path.join(testDir, 'userData');
+		fs.mkdirSync(userDataDir);
+		const extensionDir = path.join(testDir, 'extensions');
+		fs.mkdirSync(extensionDir);
+		const workspaceFolder = path.join(testDir, 'workspace');
+		fs.mkdirSync(workspaceFolder);
+
+		/**
+		 * Basic usage
+		 */
+		await runTests({
+			version: '1.53.0',
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			launchArgs: [
+				'--user-data-dir', userDataDir,
+				'--extensions-dir', extensionDir,
+				workspaceFolder
+			]
+		});
+		rimraf(testDir);
+	} catch (err) {
+		console.error('Failed to run tests');
+		process.exitCode = 1;
+	}
+}
+go();
