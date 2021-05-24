84e4f93602a3d444383090a27806f85683ccf362
Boris Jankovic <boris@tenderly.co>
2021-05-24 15:39:40 +0200

    Improve VariableDeclarationNode name location

--- parser/src/analyzer/nodes/VariableDeclarationNode.ts
+++ parser/src/analyzer/nodes/VariableDeclarationNode.ts
@@ -1,4 +1,4 @@
-import { VariableDeclaration } from "@solidity-parser/parser/dist/src/ast-types";
+import { VariableDeclaration, StateVariableDeclarationVariable } from "@solidity-parser/parser/dist/src/ast-types";
 
 import { Location, FinderType, Node } from "./Node";
 
@@ -102,26 +102,7 @@ export class VariableDeclarationNode implements Node {
             this.addTypeNode(typeNode);
             typeNode.setDeclarationNode(this);
 
-            if (this.astNode.loc && this.nameLoc && typeNode.astNode.range) {
-                const diff = 1 + (+typeNode.astNode.range[1] - +typeNode.astNode.range[0]);
-
-                this.nameLoc.start.column = this.astNode.loc.start.column + diff + 1;
-                this.nameLoc.end.column = this.nameLoc.start.column + (this.getName()?.length || 0);
-
-                if (this.astNode.visibility && this.astNode.visibility !== "default" ) {
-                    this.nameLoc.start.column += this.astNode.visibility.length + 1;
-                    this.nameLoc.end.column += this.astNode.visibility.length + 1;
-                }
-
-                if (this.astNode.storageLocation) {
-                    this.nameLoc.start.column += this.astNode.storageLocation.length + 1;
-                    this.nameLoc.end.column += this.astNode.storageLocation.length + 1;
-                }
-
-                if (this.astNode.loc.end.column < this.nameLoc.end.column) {
-                    this.astNode.loc.end.column = this.nameLoc.end.column;
-                }
-            }
+            this.updateLocationName(typeNode);
         }
 
         // Don't handle expression, it is handled in StateVariableDeclarationNode
@@ -129,5 +110,43 @@ export class VariableDeclarationNode implements Node {
         parent?.addChild(this);
 
         return this;
+    }
+
+    updateLocationName(typeNode: Node): void {
+        if (this.astNode.loc && this.nameLoc && typeNode.astNode.range) {
+            const diff = 1 + (+typeNode.astNode.range[1] - +typeNode.astNode.range[0]);
+
+            this.nameLoc.start.column = this.astNode.loc.start.column + diff + 1;
+            this.nameLoc.end.column = this.nameLoc.start.column + (this.getName()?.length || 0);
+
+            if (this.astNode.visibility && this.astNode.visibility !== "default" ) {
+                this.nameLoc.start.column += this.astNode.visibility.length + 1;
+                this.nameLoc.end.column += this.astNode.visibility.length + 1;
+            }
+
+            if (this.astNode.storageLocation) {
+                this.nameLoc.start.column += this.astNode.storageLocation.length + 1;
+                this.nameLoc.end.column += this.astNode.storageLocation.length + 1;
+            }
+
+            if (this.astNode.storageLocation) {
+                this.nameLoc.start.column += this.astNode.storageLocation.length + 1;
+                this.nameLoc.end.column += this.astNode.storageLocation.length + 1;
+            }
+
+            if (this.astNode.isDeclaredConst) {
+                this.nameLoc.start.column += "constant ".length;
+                this.nameLoc.end.column += "constant ".length;
+            }
+
+            if ((this.astNode as StateVariableDeclarationVariable).isImmutable) {
+                this.nameLoc.start.column += "immutable ".length;
+                this.nameLoc.end.column += "immutable ".length;
+            }
+
+            if (this.astNode.loc.end.column < this.nameLoc.end.column) {
+                this.astNode.loc.end.column = this.nameLoc.end.column;
+            }
+        }
     }
 }
