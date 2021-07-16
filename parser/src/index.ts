daadbce258bff6c8ea58383098baa0a10ff15c33
Boris Jankovic <boris@tenderly.co>
2021-07-16 21:25:40 +0200

    Refactor

--- parser/src/index.ts
+++
@@ -1,4 +0,0 @@
-import "module-alias/register";
-
-export { Analyzer } from "./analyzer";
-export * as types from "./analyzer/common/types";
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,10 +1,5 @@
-import * as fs from "fs";
-import * as path from "path";
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { TextDocumentIdentifier } from 'vscode-languageserver-protocol';
-
-import { Position, Range } from '../types/languageTypes';
-import { types } from "solidity-analyzer";
 
 export function getUriFromDocument(document: TextDocument | TextDocumentIdentifier): string {
     return decodeUriAndRemoveFilePrefix(document.uri);
@@ -22,20 +17,6 @@ export function decodeUriAndRemoveFilePrefix(uri: string): string {
     return uri;
 }
 
-export function findNodeModules(fromURI: string, projectRootPath: string): string | undefined {
-    let nodeModulesPath = path.join(fromURI, "..", "node_modules");
-
-    while (projectRootPath && nodeModulesPath.includes(projectRootPath) && !fs.existsSync(nodeModulesPath)) {
-        nodeModulesPath = path.join(nodeModulesPath, "..", "..", "node_modules");
-    }
-
-    if (fs.existsSync(nodeModulesPath)) {
-        return nodeModulesPath;
-    }
-
-    return undefined;
-}
-
 export function debounce<Params extends any[]>(
         func: (...args: Params) => any,
         timeoutInMilliseconds: number
@@ -49,22 +30,4 @@ export function debounce<Params extends any[]>(
             func(...args);
         }, timeoutInMilliseconds);
     };
-}
-
-export function getParserPositionFromVSCodePosition(position: Position): types.Position {
-    return {
-        // TO-DO: Remove +1 when "@solidity-parser" fix line counting.
-        // Why +1? Because "vs-code" line counting from 0, and "@solidity-parser" from 1.
-        line: position.line + 1,
-        column: position.character
-    };
-}
-
-export function getRange(loc: types.Location): Range {
-    // TO-DO: Remove -1 when "@solidity-parser" fix line counting.
-    // Why -1? Because "vs-code" line counting from 0, and "@solidity-parser" from 1.
-    return Range.create(
-        Position.create(loc.start.line - 1, loc.start.column),
-        Position.create(loc.end.line - 1, loc.end.column),
-    );
 }
