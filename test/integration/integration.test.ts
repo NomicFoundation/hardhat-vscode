a8bbb99b903521163640d9784862509a430253e8
Boris Jankovic <boris@tenderly.co>
2021-08-26 18:49:46 +0200

    Load tests from json

--- test/integration/integration.test.ts
+++ test/integration/integration.test.ts
@@ -1,5 +1,6 @@
 'use strict';
 
+import * as fs from 'fs';
 import * as path from 'path';
 import * as assert from 'assert';
 import * as vscode from 'vscode';
@@ -21,6 +22,15 @@ suite('Client integration', () => {
 	let middleware: lsclient.Middleware;
 	let tokenSource!: vscode.CancellationTokenSource;
 	let docUri!: vscode.Uri;
+
+	const integrationSamples = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', '..', 'test', 'integration', 'integration.test.json'), 'utf8'));
+	const integrationTests = {
+		doDefinitionRequest,
+		doTypeDefinitionRequest,
+		doReferencesRequest,
+		doImplementationRequest,
+		doRenameRequest,
+	};
 
 	suiteSetup(async () => {
 		docUri = getDocUri('test.sol');
@@ -95,136 +105,150 @@ suite('Client integration', () => {
 		assert.deepStrictEqual(client.initializeResult, expected);
 	});
 
-	test('Goto Definition', async () => {
-		docUri = getDocUri('test1.sol');
-		await changeDocument(docUri);
+	for (const sample of integrationSamples) {
+		test(sample.title, async () => {
+			docUri = getDocUri(sample.uri);
+			await changeDocument(docUri);
 
-		const provider = client.getFeature(lsclient.DefinitionRequest.method).getProvider(document);
-		isDefined(provider);
+			const fn = integrationTests[`do${sample.action}`];
+			if (!fn) {
+				throw new Error("Action request not implemented!");
+			}
 
-		const position: vscode.Position = new vscode.Position(67, 14);
-		const result = (await provider.provideDefinition(document, position, tokenSource.token)) as vscode.Location;
+			await fn(client, tokenSource, sample);
+		});
+	}
+});
+
+async function doDefinitionRequest(client: lsclient.LanguageClient, tokenSource: vscode.CancellationTokenSource, sample: any): Promise<void> {
+	const provider = client.getFeature(lsclient.DefinitionRequest.method).getProvider(document);
+	isDefined(provider);
+
+	const position = new vscode.Position(sample.position.line, sample.position.character);
+	const result = (await provider.provideDefinition(document, position, tokenSource.token)) as vscode.Location;
+
+	isInstanceOf(result, vscode.Location);
+	uriEqual(result.uri, sample.expected.uri);
+	rangeEqual(
+		result.range,
+		sample.expected.range[0].line,
+		sample.expected.range[0].character,
+		sample.expected.range[1].line,
+		sample.expected.range[1].character
+	);
+}
+
+async function doTypeDefinitionRequest(client: lsclient.LanguageClient, tokenSource: vscode.CancellationTokenSource, sample: any): Promise<void> {
+	const provider = client.getFeature(lsclient.TypeDefinitionRequest.method).getProvider(document);
+	isDefined(provider);
+
+	const position = new vscode.Position(sample.position.line, sample.position.character);
+	const results = (await provider.provideTypeDefinition(document, position, tokenSource.token)) as vscode.Location[];
+
+	isArray(results, vscode.Location, sample.expected.length);
+	for (let i = 0; i < results.length; i++) {
+		const result = results[i];
+		const expected = sample.expected[i];
 
 		isInstanceOf(result, vscode.Location);
-		uriEqual(result.uri, docUri);
-		rangeEqual(result.range, 96, 0, 113, 0);
-	});
+		uriEqual(result.uri, expected.uri);
+		rangeEqual(
+			result.range,
+			expected.range[0].line,
+			expected.range[0].character,
+			expected.range[1].line,
+			expected.range[1].character
+		);
+	}
+}
 
-	test('Goto Type Definition', async () => {
-		docUri = getDocUri('test.sol');
-		await changeDocument(docUri);
+async function doReferencesRequest(client: lsclient.LanguageClient, tokenSource: vscode.CancellationTokenSource, sample: any): Promise<void> {
+	const provider = client.getFeature(lsclient.ReferencesRequest.method).getProvider(document);
+	isDefined(provider);
 
-		const provider = client.getFeature(lsclient.TypeDefinitionRequest.method).getProvider(document);
-		isDefined(provider);
+	const position = new vscode.Position(sample.position.line, sample.position.character);
+	const results = (await provider.provideReferences(
+		document,
+		position,
+		{
+			includeDeclaration: true
+		},
+		tokenSource.token
+	)) as vscode.Location[];
 
-		const position: vscode.Position = new vscode.Position(41, 16);
-		const results = (await provider.provideTypeDefinition(document, position, tokenSource.token)) as vscode.Location[];
+	isArray(results, vscode.Location, sample.expected.length);
+	for (let i = 0; i < results.length; i++) {
+		const result = results[i];
+		const expected = sample.expected[i];
 
-		isArray(results, vscode.Location);
-		for (const result of results) {
-			isInstanceOf(result, vscode.Location);
-			uriEqual(result.uri, docUri);
-			rangeEqual(result.range, 15, 11, 15, 19);
+		isInstanceOf(result, vscode.Location);
+		uriEqual(result.uri, expected.uri);
+		rangeEqual(
+			result.range,
+			expected.range[0].line,
+			expected.range[0].character,
+			expected.range[1].line,
+			expected.range[1].character
+		);
+	}
+}
+
+async function doImplementationRequest(client: lsclient.LanguageClient, tokenSource: vscode.CancellationTokenSource, sample: any): Promise<void> {
+	const provider = client.getFeature(lsclient.ImplementationRequest.method).getProvider(document);
+	isDefined(provider);
+
+	const position = new vscode.Position(sample.position.line, sample.position.character);
+	const results = (await provider.provideImplementation(
+		document,
+		position,
+		tokenSource.token
+	)) as vscode.Location[];
+	
+	isArray(results, vscode.Location, sample.expected.length);
+	for (let i = 0; i < results.length; i++) {
+		const result = results[i];
+		const expected = sample.expected[i];
+
+		isInstanceOf(result, vscode.Location);
+		uriEqual(result.uri, expected.uri);
+		rangeEqual(
+			result.range,
+			expected.range[0].line,
+			expected.range[0].character,
+			expected.range[1].line,
+			expected.range[1].character
+		);
+	}
+}
+
+// TO-DO: Improve expected sample
+async function doRenameRequest(client: lsclient.LanguageClient, tokenSource: vscode.CancellationTokenSource, sample: any): Promise<void> {
+	const provider = client.getFeature(lsclient.RenameRequest.method).getProvider(document);
+	isDefined(provider);
+
+	const position = new vscode.Position(sample.position.line, sample.position.character);
+	const renameResult = await provider.provideRenameEdits(document, position, 'newName', tokenSource.token);
+
+	isInstanceOf(renameResult, vscode.WorkspaceEdit);
+	for (const results of renameResult.entries()) {
+		if (results.length !== 2) {
+			throw new Error(`Result [vscode.Uri, vscode.TextEdit[]].length must be 2`);
 		}
-	});
 
-	test('Find References', async () => {
-		docUri = getDocUri('test.sol');
-		await changeDocument(docUri);
+		isInstanceOf(results[0], vscode.Uri);
+		uriEqual(results[0], sample.expected[0]);
 
-		const expectedResults: vscode.Location[] = JSON.parse('[{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":27,"character":22},{"line":27,"character":31}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":41,"character":12},{"line":41,"character":21}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":104,"character":12},{"line":104,"character":21}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":124,"character":8},{"line":124,"character":17}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":133,"character":29},{"line":133,"character":38}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":134,"character":16},{"line":134,"character":25}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":135,"character":35},{"line":135,"character":44}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},"range":[{"line":147,"character":22},{"line":147,"character":31}]}]');
-
-		const provider = client.getFeature(lsclient.ReferencesRequest.method).getProvider(document);
-		isDefined(provider);
-
-		const position: vscode.Position = new vscode.Position(41, 16);
-		const results = (await provider.provideReferences(
-			document,
-			position,
-			{
-				includeDeclaration: true
-			},
-			tokenSource.token
-		)) as vscode.Location[];
-
-		isArray(results, vscode.Location, 8);
-
-		for (let i = 0; i < results.length; i++) {
-			isInstanceOf(results[i], vscode.Location);
-			uriEqual(results[i].uri, expectedResults[i].uri);
+		const textEdits = results[1];
+		for (let i = 0; i < textEdits.length; i++) {
+			isInstanceOf(textEdits[i], vscode.TextEdit);
+			assert.strictEqual(textEdits[i].newText, sample.expected[1][i].newText);
 			rangeEqual(
-				results[i].range,
-				expectedResults[i].range[0].line,
-				expectedResults[i].range[0].character,
-				expectedResults[i].range[1].line,
-				expectedResults[i].range[1].character
+				textEdits[i].range,
+				sample.expected[1][i].range[0].line,
+				sample.expected[1][i].range[0].character,
+				sample.expected[1][i].range[1].line,
+				sample.expected[1][i].range[1].character
 			);
 		}
-	});
-
-	test('Find Implementations', async () => {
-		docUri = getDocUri('test1.sol');
-		await changeDocument(docUri);
-
-		const expectedResults: vscode.Location[] = JSON.parse('[{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test1.sol","scheme":"file"},"range":[{"line":11,"character":9},{"line":11,"character":10}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test1.sol","scheme":"file"},"range":[{"line":33,"character":14},{"line":33,"character":15}]},{"uri":{"$mid":1,"path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test1.sol","scheme":"file"},"range":[{"line":48,"character":14},{"line":48,"character":15}]}]');
-
-		const provider = client.getFeature(lsclient.ImplementationRequest.method).getProvider(document);
-		isDefined(provider);
-
-		const position: vscode.Position = new vscode.Position(11, 10);
-		const results = (await provider.provideImplementation(
-			document,
-			position,
-			tokenSource.token
-		)) as vscode.Location[];
-		
-		isArray(results, vscode.Location, 3);
-
-		for (let i = 0; i < results.length; i++) {
-			isInstanceOf(results[i], vscode.Location);
-			uriEqual(results[i].uri, expectedResults[i].uri);
-			rangeEqual(
-				results[i].range,
-				expectedResults[i].range[0].line,
-				expectedResults[i].range[0].character,
-				expectedResults[i].range[1].line,
-				expectedResults[i].range[1].character
-			);
-		}
-	});
-
-	test('Do Rename', async () => {
-		docUri = getDocUri('test.sol');
-		await changeDocument(docUri);
-
-		const expectedResults: [vscode.Uri, vscode.TextEdit[]] = JSON.parse('[{"$mid":1,"external":"file:///Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","path":"/Users/riphal/Documents/Tenderly/vscode-solidity/test/testdata/test.sol","scheme":"file"},[{"range":[{"line":27,"character":22},{"line":27,"character":31}],"newText":"newName"},{"range":[{"line":41,"character":12},{"line":41,"character":21}],"newText":"newName"},{"range":[{"line":104,"character":12},{"line":104,"character":21}],"newText":"newName"},{"range":[{"line":124,"character":8},{"line":124,"character":17}],"newText":"newName"},{"range":[{"line":133,"character":29},{"line":133,"character":38}],"newText":"newName"},{"range":[{"line":134,"character":16},{"line":134,"character":25}],"newText":"newName"},{"range":[{"line":135,"character":35},{"line":135,"character":44}],"newText":"newName"},{"range":[{"line":147,"character":22},{"line":147,"character":31}],"newText":"newName"}]]');
-		const provider = client.getFeature(lsclient.RenameRequest.method).getProvider(document);
-		isDefined(provider);
-
-		const position: vscode.Position = new vscode.Position(41, 16);
-		const renameResult = await provider.provideRenameEdits(document, position, 'newName', tokenSource.token);
-
-		isInstanceOf(renameResult, vscode.WorkspaceEdit);
-		for (const results of renameResult.entries()) {
-			if (results.length !== 2) {
-				throw new Error(`Result [vscode.Uri, vscode.TextEdit[]].length must be 2`);
-			}
-
-			isInstanceOf(results[0], vscode.Uri);
-			uriEqual(results[0], expectedResults[0]);
-
-			const textEdits = results[1];
-			for (let i = 0; i < textEdits.length; i++) {
-				isInstanceOf(textEdits[i], vscode.TextEdit);
-				assert.strictEqual(textEdits[i].newText, expectedResults[1][i].newText);
-				rangeEqual(
-					textEdits[i].range,
-					expectedResults[1][i].range[0].line,
-					expectedResults[1][i].range[0].character,
-					expectedResults[1][i].range[1].line,
-					expectedResults[1][i].range[1].character
-				);
-			}
-		}
-	});
-});
+	}
+}
