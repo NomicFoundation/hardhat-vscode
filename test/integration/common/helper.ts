4da28963f3d6b52d93c391975a6dfc97d9c5cd37
Boris Jankovic <boris@tenderly.co>
2021-09-07 00:10:57 +0200

    Refactor helper

--- test/integration/common/helper.ts
+++ test/integration/common/helper.ts
@@ -1,48 +1,11 @@
-import * as path from 'path';
+'use strict';
+
 import * as vscode from 'vscode';
 import * as assert from 'assert';
 
-export let document: vscode.TextDocument;
-export let editor: vscode.TextEditor;
-
-/**
- * Activates the tenderly.solidity-extension extension
- */
-export async function activate(docUri: vscode.Uri) {
-	// The extensionId is `publisher.name` from package.json
-	const ext = vscode.extensions.getExtension('tenderly.solidity-extension')!;
-	await ext.activate();
-
-	try {
-		document = await vscode.workspace.openTextDocument(docUri);
-		editor = await vscode.window.showTextDocument(document);
-
-		await sleep(2000); // Wait for server activation
-	} catch (e) {
-		console.error(e);
-	}
-}
-
-export async function changeDocument(docUri: vscode.Uri) {
-	try {
-		document = await vscode.workspace.openTextDocument(docUri);
-		editor = await vscode.window.showTextDocument(document);
-	} catch (e) {
-		console.error(e);
-	}
-}
-
-async function sleep(ms: number) {
+export async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-export const getDocPath = (p: string) => {
-	return path.resolve(__dirname, '../../../test/testdata', p);
-};
-
-export const getDocUri = (p: string) => {
-	return vscode.Uri.file(getDocPath(p));
-};
 
 export function rangeEqual(range: vscode.Range, sl: number, sc: number, el: number, ec: number): void {
     assert.strictEqual(range.start.line, sl);
@@ -52,8 +15,8 @@ export function rangeEqual(range: vscode.Range, sl: number, sc: number, el: numb
 }
 
 export function uriEqual(actual: vscode.Uri, expected: vscode.Uri): void {
-	const actualPath = actual.path.match(/test\/testdata(.*)/)[0];
-	const expectedPath = expected.path.match(/test\/testdata(.*)/)[0];
+	const actualPath = actual.path.match(/testdata(.*)/)[0];
+	const expectedPath = expected.path.match(/testdata(.*)/)[0];
 
     assert.strictEqual(actualPath, expectedPath);
 }
