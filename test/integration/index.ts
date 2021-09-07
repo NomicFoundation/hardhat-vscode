b4a9489de675c70c69f95989e0ca6c1a258e8439
Boris Jankovic <boris@tenderly.co>
2021-09-07 15:56:58 +0200

    Increase mocha timeout

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -7,7 +7,7 @@ export function run(): Promise<void> {
 	const mocha = new Mocha({
 		ui: 'tdd',
 		color: true,
-		timeout: 20000
+		timeout: 30000
 	});
 
 	const testsRoot = path.resolve(__dirname, "tests");
