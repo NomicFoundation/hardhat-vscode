0b49b4f41262aeb9057c04c3dcab34fc19b6fcc7
Boris Jankovic <boris@tenderly.co>
2021-09-14 09:39:29 +0200

    Handle RevertStatmentNode

--- server/src/parser/analyzer/nodes/RevertStatementNode.ts
+++ server/src/parser/analyzer/nodes/RevertStatementNode.ts
@@ -6,12 +6,13 @@ export class RevertStatementNode extends Node {
     constructor (revertStatement: RevertStatement, uri: string, rootPath: string, documentsAnalyzer: DocumentsAnalyzerMap) {
         super(revertStatement, uri, rootPath, documentsAnalyzer, undefined);
         this.astNode = revertStatement;
-        // TO-DO: Implement name location for rename
     }
 
     accept(find: FinderType, orphanNodes: Node[], parent?: Node, expression?: Node): Node {
         this.setExpressionNode(expression);
-        // TO-DO: Method not implemented
+
+        find(this.astNode.revertCall, this.uri, this.rootPath, this.documentsAnalyzer).accept(find, orphanNodes, parent, this);
+
         return this;
     }
 }
