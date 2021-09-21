0116f5d41d5786b6a9242c01234458c8061a6a12
Boris Jankovic <boris@tenderly.co>
2021-09-21 18:13:32 +0200

    Added tests for jump to import file

---
+++ test/integration/tests/import-navigation/importNavigation.test.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+import { runTestFromJSON } from '../utils';
+
+suite('Jump to import file navigation', function () {
+	this.timeout(10000);
+	runTestFromJSON(__dirname, 'importNavigation.test.json');
+});
