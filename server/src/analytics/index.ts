e95b5409bd24df7e176b0a5686a953abfd97b936
Boris Jankovic <boris@tenderly.co>
2021-11-20 11:27:43 +0100

    Removed console.log

--- server/src/analytics/index.ts
+++ server/src/analytics/index.ts
@@ -112,7 +112,7 @@ class GoogleAnalytics implements Analytics {
                 'Content-Type': 'text/plain;charset=UTF-8'
             },
             body: hitPayload
-        }).then(data => console.log(data));
+        });
 	}
 }
 
