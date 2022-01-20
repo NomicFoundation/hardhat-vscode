4eda326dd46ac5e7d4446412c0dbdafeae4d2420
John Kane <john@kanej.me>
2022-01-20 16:53:12 +0000

    feat(server): quickfix for adding `abstract` to contract
    This quickfix adds the `abstract` keyword to the start of a contract in
    response to `solidity 3656`.
    
    The error is constrained to the contracts name as well.

--- server/src/compilerDiagnostics/conversions/attemptConstrainToContractName.ts
+++ server/src/compilerDiagnostics/conversions/attemptConstrainToContractName.ts
@@ -2,9 +2,13 @@ import { TextDocument, Diagnostic } from "@common/types";
 import { HardhatCompilerError } from "../types";
 import { constrainByRegex } from "./constrainByRegex";
 
-export function attemptConstrainToFunctionName(
+export function attemptConstrainToContractName(
   document: TextDocument,
   error: HardhatCompilerError
 ): Diagnostic {
-  return constrainByRegex(document, error, /(?<=function\s+)[^\s]+(?=\s*\()/gm);
+  return constrainByRegex(
+    document,
+    error,
+    /(?<=contract\s+)[^\s]+(?=\s*(is|{))/gm
+  );
 }
---
+++ server/src/compilerDiagnostics/conversions/constrainByRegex.ts
@@ -0,0 +1,58 @@
+import { TextDocument, Range } from "@common/types";
+import { HardhatCompilerError } from "../types";
+import { passThroughConversion } from "./passThroughConversion";
+
+export function constrainByRegex(
+  document: TextDocument,
+  error: HardhatCompilerError,
+  regex: RegExp
+) {
+  const diagnostic = passThroughConversion(document, error);
+
+  const matchResult = findMatchInRange(document, error.sourceLocation, regex);
+
+  if (!matchResult) {
+    return diagnostic;
+  }
+
+  const updatedRange = Range.create(
+    document.positionAt(error.sourceLocation.start + matchResult.index),
+    document.positionAt(
+      error.sourceLocation.start + matchResult.index + matchResult.text.length
+    )
+  );
+
+  return {
+    ...diagnostic,
+    range: updatedRange,
+    data: {
+      functionSourceLocation: {
+        start: error.sourceLocation.start,
+        end: error.sourceLocation.end,
+      },
+    },
+  };
+}
+
+function findMatchInRange(
+  document: TextDocument,
+  sourceLocation: { start: number; end: number },
+  regex: RegExp
+) {
+  const range = Range.create(
+    document.positionAt(sourceLocation.start),
+    document.positionAt(sourceLocation.end)
+  );
+
+  const text = document.getText(range);
+
+  const match = regex.exec(text);
+
+  if (!match) {
+    return null;
+  }
+
+  const [matchedText] = match;
+
+  return { index: match.index, text: matchedText };
+}
