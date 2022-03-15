154847d2d0b9742269cf901055f70292cddb9938
John Kane <john@kanej.me>
2022-03-15 15:33:00 +0000

    chore(validation): improve error logging
    Improve the logging of validation process failures (not the compiler
    warning/errors themselve but the process) but passing errors caught on
    the validation child process back to the main process and logging them.
    
    They are passed back as serialized json and not error objects.

--- server/src/parser/services/validation/events.ts
+++ server/src/parser/services/validation/events.ts
@@ -5,3 +5,4 @@ export const SOLIDITY_COMPILE_CONFIRMATION_EVENT =
 export const COMPILER_DOWNLOADED_EVENT = "compiler_downloaded";
 export const SOLIDITY_COMPILE_EVENT = "solidity_compile";
 export const HARDHAT_CONFIG_FILE_EXIST_EVENT = "hardhat_config_file_exist";
+export const HARDHAT_PROCESS_ERROR = "error";
