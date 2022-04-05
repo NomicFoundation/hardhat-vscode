1792430122dcd45642dc1020ddf1ef888c699c2a
John Kane <john@kanej.me>
2022-04-05 15:08:20 +0100

    refactor(server): clean up initialization

--- server/src/services/initialization/onInitialize.ts
+++ server/src/services/initialization/onInitialize.ts
@@ -12,46 +12,10 @@ export const onInitialize = (serverState: ServerState) => {
     logger.trace("onInitialize");
     logger.info("Language server starting");
 
-    serverState.env = params.initializationOptions?.env ?? "production";
+    const { machineId, extensionName, extensionVersion } =
+      getSessionInfo(params);
 
-    serverState.globalTelemetryEnabled =
-      params.initializationOptions?.globalTelemetryEnabled ?? false;
-    serverState.hardhatTelemetryEnabled =
-      params.initializationOptions?.hardhatTelemetryEnabled ?? false;
-
-    const machineId: string | undefined =
-      params.initializationOptions?.machineId;
-    const extensionName: string | undefined =
-      params.initializationOptions?.extensionName;
-    const extensionVersion: string | undefined =
-      params.initializationOptions?.extensionVersion;
-
-    serverState.workspaceFolders = params.workspaceFolders ?? [];
-
-    serverState.hasWorkspaceFolderCapability = !!(
-      params.capabilities.workspace &&
-      !!params.capabilities.workspace.workspaceFolders
-    );
-
-    logger.info(`  Release: ${extensionName}@${extensionVersion}`);
-    logger.info(`  Environment: ${serverState.env}`);
-    logger.info(`  Telemetry Enabled: ${serverState.globalTelemetryEnabled}`);
-    if (machineId) {
-      logger.info(
-        `  Telemetry Tracking Id: ${
-          machineId.length > 10 ? machineId.substring(0, 10) + "..." : machineId
-        }`
-      );
-    }
-
-    if (serverState.workspaceFolders.length === 0) {
-      logger.info(`  Workspace Folders: none`);
-    } else {
-      logger.info(`  Workspace Folders:`);
-      for (const folder of serverState.workspaceFolders) {
-        logger.info(`    ${folder.name} (${folder.uri})`);
-      }
-    }
+    updateServerStateFromParams(serverState, params);
 
     serverState.telemetry.init(
       machineId,
@@ -59,6 +23,12 @@ export const onInitialize = (serverState: ServerState) => {
       extensionVersion,
       serverState
     );
+
+    logInitializationInfo(serverState, {
+      machineId,
+      extensionName,
+      extensionVersion,
+    });
 
     const result: InitializeResult = {
       capabilities: {
@@ -91,3 +61,67 @@ export const onInitialize = (serverState: ServerState) => {
     return result;
   };
 };
+
+function getSessionInfo(params: InitializeParams) {
+  const machineId: string | undefined = params.initializationOptions?.machineId;
+  const extensionName: string | undefined =
+    params.initializationOptions?.extensionName;
+  const extensionVersion: string | undefined =
+    params.initializationOptions?.extensionVersion;
+
+  return { machineId, extensionName, extensionVersion };
+}
+
+function updateServerStateFromParams(
+  serverState: ServerState,
+  params: InitializeParams
+) {
+  serverState.env = params.initializationOptions?.env ?? "production";
+
+  serverState.globalTelemetryEnabled =
+    params.initializationOptions?.globalTelemetryEnabled ?? false;
+  serverState.hardhatTelemetryEnabled =
+    params.initializationOptions?.hardhatTelemetryEnabled ?? false;
+
+  serverState.workspaceFolders = params.workspaceFolders ?? [];
+
+  serverState.hasWorkspaceFolderCapability = !!(
+    params.capabilities.workspace &&
+    !!params.capabilities.workspace.workspaceFolders
+  );
+}
+
+function logInitializationInfo(
+  serverState: ServerState,
+  {
+    machineId,
+    extensionName,
+    extensionVersion,
+  }: {
+    machineId: string | undefined;
+    extensionName: string | undefined;
+    extensionVersion: string | undefined;
+  }
+) {
+  const { logger } = serverState;
+
+  logger.info(`  Release: ${extensionName}@${extensionVersion}`);
+  logger.info(`  Environment: ${serverState.env}`);
+  logger.info(`  Telemetry Enabled: ${serverState.globalTelemetryEnabled}`);
+  if (machineId) {
+    logger.info(
+      `  Telemetry Tracking Id: ${
+        machineId.length > 10 ? machineId.substring(0, 10) + "..." : machineId
+      }`
+    );
+  }
+
+  if (serverState.workspaceFolders.length === 0) {
+    logger.info(`  Workspace Folders: none`);
+  } else {
+    logger.info(`  Workspace Folders:`);
+    for (const folder of serverState.workspaceFolders) {
+      logger.info(`    ${folder.name} (${folder.uri})`);
+    }
+  }
+}
