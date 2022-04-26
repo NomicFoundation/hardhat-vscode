890aa5de658ab0dbbca8d23e60917535e856832b
John Kane <john@kanej.me>
2022-04-26 12:24:21 +0100

    fix(indexing): add workspace on indexing

--- server/test/helpers/setupMockWorkspaceFileRetriever.ts
+++ server/test/helpers/setupMockWorkspaceFileRetriever.ts
@@ -1,12 +1,17 @@
 import { WorkspaceFileRetriever } from "@analyzer/WorkspaceFileRetriever";
 
 export function setupMockWorkspaceFileRetriever(
-  projects: string[] = []
+  projects: string[] = [],
+  solFiles: string[] = []
 ): WorkspaceFileRetriever {
   return {
     findFiles: async (_baseUri: string, globPattern: string) => {
       if (globPattern === "**/hardhat.config.{ts,js}") {
         return projects;
+      }
+
+      if (globPattern === "**/*.sol") {
+        return solFiles;
       }
 
       return [];
