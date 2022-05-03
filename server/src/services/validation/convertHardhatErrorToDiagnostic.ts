3597dece358645c3d1ea88e1fb489b5d7ecc8e26
John Kane <john@kanej.me>
2022-05-03 17:08:18 +0100

    feat(validation): display hh import line errors
    During validation if a Hardhat error is thrown for the import line (i.e.
    HH errors 404, 405, 406, 407, 408 or 409), then convert that into a
    diagnostic and show it as we do for solc warnings/errors.
    
    Fixes #154.

---
+++ server/src/services/validation/convertHardhatErrorToDiagnostic.ts
@@ -0,0 +1,83 @@
+import { Diagnostic, DiagnosticSeverity, Range } from "@common/types";
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+type ImportLineErrorCode = 404 | 405 | 406 | 407 | 408 | 409;
+const IMPORT_LINE_ERROR_CODES = [404, 405, 406, 407, 408, 409];
+
+type HardhatImportLineError = {
+  errorDescriptor: {
+    number: ImportLineErrorCode;
+    title: string;
+    description: string;
+  };
+  messageArguments: {
+    imported: string;
+  };
+};
+
+type UnknownHardhatError = {
+  errorDescriptor: {
+    number: number;
+    title: string;
+    description: string;
+  };
+};
+
+type HardhatError = UnknownHardhatError | HardhatImportLineError;
+
+function isHardhatImportLineError(
+  error: HardhatError
+): error is HardhatImportLineError {
+  return IMPORT_LINE_ERROR_CODES.includes(error.errorDescriptor.number);
+}
+
+export function convertHardhatErrorToDiagnostic(
+  document: TextDocument,
+  err: HardhatError
+): Diagnostic | null {
+  if (!isHardhatImportLineError(err)) {
+    return null;
+  }
+
+  return resolveImportLineError(document, err, err.errorDescriptor.title);
+}
+
+function resolveImportLineError(
+  document: TextDocument,
+  err: HardhatImportLineError,
+  message: string
+) {
+  const range = findRangeForImport(document, err);
+
+  if (!range) {
+    return null;
+  }
+
+  return {
+    severity: DiagnosticSeverity.Error,
+    code: err.errorDescriptor.number,
+    source: "hardhat",
+    message,
+    range,
+  };
+}
+
+function findRangeForImport(
+  document: TextDocument,
+  err: HardhatImportLineError
+): Range | null {
+  const imported = err.messageArguments.imported;
+
+  const startIndex = document.getText().indexOf(imported);
+
+  if (startIndex === -1) {
+    return null;
+  }
+
+  const endIndex = startIndex + imported.length;
+
+  return {
+    start: document.positionAt(startIndex),
+    end: document.positionAt(endIndex),
+  };
+}
