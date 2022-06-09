0f400f54550e3106700c5fedd9c9d4f222e37023
John Kane <john@kanej.me>
2022-06-09 16:49:45 +0100

    feat: enforce input settings for validation (#197)
    For validation we do not need solc code generation, just errors and
    warnings. We override these setting no matter what the user has in
    their `hardhat.config.{ts,js}`.
    
    Other settings are passed on unchanged.
    
    This does mean we will not display errors that come from the byte
    code generation phase (i.e. solidity(5574)).

--- server/src/services/validation/worker/build/solcCompile.ts
+++ server/src/services/validation/worker/build/solcCompile.ts
@@ -20,14 +20,25 @@ export async function solcCompile(
   { input, solcBuild }: SolcInput
 ): Promise<SolcResult> {
   let output;
+
+  const originalInput = input as { settings: {} };
+
+  const overriddenInput = {
+    ...originalInput,
+    settings: {
+      ...originalInput.settings,
+      outputSelection: {},
+    },
+  };
+
   if (solcBuild.isSolcJs) {
     output = await hre.run(TASK_COMPILE_SOLIDITY_RUN_SOLCJS, {
-      input,
+      input: overriddenInput,
       solcJsPath: solcBuild.compilerPath,
     });
   } else {
     output = await hre.run(TASK_COMPILE_SOLIDITY_RUN_SOLC, {
-      input,
+      input: overriddenInput,
       solcPath: solcBuild.compilerPath,
     });
   }
