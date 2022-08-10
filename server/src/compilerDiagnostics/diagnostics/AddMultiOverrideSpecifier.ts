87110fd15b092b2c335e2fedeff143021911d960
Armando Andini <armando.andini@gmail.com>
2022-08-10 11:30:10 -0300

    feat: Quickfix for partially-specified multi override
    We had quickfix in place for functions not having the override specifier
    but it was not working for cases where
    the override keyword was used alone or with incomplete parameters

--- server/src/compilerDiagnostics/diagnostics/AddMultiOverrideSpecifier.ts
+++ server/src/compilerDiagnostics/diagnostics/AddMultiOverrideSpecifier.ts
@@ -1,4 +1,8 @@
-import { CodeAction, Diagnostic } from "vscode-languageserver/node";
+import {
+  CodeAction,
+  CodeActionKind,
+  Diagnostic,
+} from "vscode-languageserver/node";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import { CompilerDiagnostic, ResolveActionsContext } from "../types";
 import { attemptConstrainToFunctionName } from "../conversions/attemptConstrainToFunctionName";
@@ -7,6 +11,8 @@ import {
   Multioverride,
   resolveInsertSpecifierQuickFix,
 } from "./common/resolveInsertSpecifierQuickFix";
+
+const OVERRIDE_REGEX = /^override\s*(\((.*)\))?$/;
 
 export class AddMultiOverrideSpecifier implements CompilerDiagnostic {
   public code = "4327";
@@ -30,12 +36,33 @@ export class AddMultiOverrideSpecifier implements CompilerDiagnostic {
 
     const multiOverride = new Multioverride(missingContractIdentifiers);
 
-    return resolveInsertSpecifierQuickFix(
+    // When no specifier is present, solc sourceLocation covers the whole function body
+    // so we can apply the general quickfix for specifiers
+    const insertSpecifierQuickfix = resolveInsertSpecifierQuickFix(
       multiOverride,
       diagnostic,
       context,
       serverState.logger
     );
+
+    if (insertSpecifierQuickfix.length > 0) {
+      return insertSpecifierQuickfix;
+    }
+
+    // If just 'override' or 'override(A)' is present, assuming there are more
+    // contracts that need to be specified besides A, solc error sourceLocation only
+    // covers the `override(...)` string
+    const updateSpecifierQuickfix = this._tryUpdateOverride(
+      context,
+      diagnostic,
+      multiOverride
+    );
+
+    if (updateSpecifierQuickfix.length > 0) {
+      return updateSpecifierQuickfix;
+    }
+
+    return [];
   }
 
   public parseContractIdentifiersFromMessage(message: string) {
@@ -48,5 +75,50 @@ export class AddMultiOverrideSpecifier implements CompilerDiagnostic {
       .filter((contract): contract is string => contract !== undefined);
 
     return missingContractIdentifiers;
+  }
+
+  private _isOverrideSpecifier(specifier: string): boolean {
+    return OVERRIDE_REGEX.test(specifier);
+  }
+
+  private _getOverridenContracts(specifier: string): string[] {
+    const match = specifier.match(OVERRIDE_REGEX);
+    const overridenContractString = (match && (match[2] as string)) || "";
+    return overridenContractString
+      .split(/\s*,\s*/)
+      .map((s) => s.trim())
+      .filter((s) => s);
+  }
+
+  private _tryUpdateOverride(
+    context: ResolveActionsContext,
+    diagnostic: Diagnostic,
+    multiOverride: Multioverride
+  ) {
+    const sourceError = context.document.getText(diagnostic.range);
+    if (this._isOverrideSpecifier(sourceError)) {
+      const overridenContracts = this._getOverridenContracts(sourceError);
+      multiOverride.contractIdentifiers.push(...overridenContracts);
+
+      return [
+        {
+          title: "Add missing contracts to specifier",
+          kind: CodeActionKind.QuickFix,
+          isPreferred: true,
+          edit: {
+            changes: {
+              [context.uri]: [
+                {
+                  range: diagnostic.range,
+                  newText: multiOverride.toString(),
+                },
+              ],
+            },
+          },
+        },
+      ];
+    } else {
+      return [];
+    }
   }
 }
--- server/test/services/codeactions/testData/AddMultioverrideSpecifier.sol
+++ server/test/services/codeactions/testData/AddMultioverrideSpecifier.sol
@@ -2,58 +2,66 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Alpha {
-  function foo() public virtual {}
+    function foo() public virtual {}
 
-  function bar() public virtual {}
+    function bar() public virtual {}
 
-  function longFunctionName()
-    public
-    virtual
-    mathsWorks
-    returns (string memory outputlong)
-  {}
+    function baz() public virtual {}
 
-  modifier mathsWorks() {
-    require(25 == 25, "happy");
-    _;
-  }
+    function longFunctionName()
+        public
+        virtual
+        mathsWorks
+        returns (string memory outputlong)
+    {}
+
+    modifier mathsWorks() {
+        require(25 == 25, "happy");
+        _;
+    }
 }
 
 contract Beta {
-  function bar() public virtual {}
+    function bar() public virtual {}
 
-  function longFunctionName()
-    public
-    virtual
-    returns (string memory outputlong)
-  {}
+    function baz() public virtual {}
+
+    function longFunctionName()
+        public
+        virtual
+        returns (string memory outputlong)
+    {}
 }
 
 contract Gamma {
-  function foo() public virtual {}
+    function foo() public virtual {}
 
-  function bar() public virtual {}
+    function bar() public virtual {}
 
-  function longFunctionName()
-    public
-    virtual
-    returns (string memory outputlong)
-  {}
+    function baz() public virtual {}
+
+    function longFunctionName()
+        public
+        virtual
+        returns (string memory outputlong)
+    {}
 }
 
 contract Omega is Alpha, Beta, Gamma {
-  function foo() {}
+    function foo() {}
 
-  function bar() public view virtual {}
+    function bar() public view virtual {}
 
-  function longFunctionName()
-    public
-    virtual
-    returns (string memory outputlong)
-  {}
+    function baz() public virtual override(Alpha) {}
 
-  modifier anotherMod() {
-    require(25 == 25, "happy");
-    _;
-  }
+    function longFunctionName()
+        public
+        virtual
+        returns (string memory outputlong)
+    {}
+
+    modifier anotherMod() {
+        require(25 == 25, "happy");
+        _;
+    }
 }
