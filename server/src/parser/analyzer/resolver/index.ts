82eee1b49bf150ebdb8dbc673a3063d1415d9a5e
Sebastian Siemssen <fubhy@fubhy.com>
2022-08-18 15:29:58 +0200

    refactor: simplify resolveDependency

--- server/src/parser/analyzer/resolver/index.ts
+++ server/src/parser/analyzer/resolver/index.ts
@@ -3,27 +3,21 @@ import path from "node:path";
 import { Remapping } from "@common/types";
 import { toUnixStyle } from "../../../utils";
 
-function applyPathRemapping(originalPath: string, pathRemappings: Remapping[]) {
-  if (!pathRemappings.length || originalPath.startsWith(".")) {
-    return originalPath;
-  }
-
-  for (const { from, to } of pathRemappings) {
-    if (originalPath.startsWith(from)) {
-      return path.join(to, originalPath.slice(from.length));
-    }
-  }
-
-  return originalPath;
-}
-
 export function resolveDependency(
   cwd: string,
   originalPath: string,
   pathRemappings: Remapping[] = []
 ): string {
-  const remappedPath = applyPathRemapping(originalPath, pathRemappings);
-  const resolvedPath = require.resolve(remappedPath, {
+  if (pathRemappings.length && !originalPath.startsWith(".")) {
+    for (const { from, to } of pathRemappings) {
+      if (originalPath.startsWith(from)) {
+        const remappedPath = path.join(to, originalPath.slice(from.length));
+        return toUnixStyle(fs.realpathSync(remappedPath));
+      }
+    }
+  }
+
+  const resolvedPath = require.resolve(originalPath, {
     paths: [fs.realpathSync(cwd)],
   });
 
