e1f3d0219be95dbb294b6b91b13d3cb5f79b6b6f
Armando Andini <armando.andini@gmail.com>
2022-12-08 09:50:09 -0300

    migrate remapping integration tests

--- test/protocol/test/textDocument/definition/foundry/definition.test.ts
+++ test/protocol/test/textDocument/definition/foundry/definition.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import { test } from 'mocha'
 import { toUri } from '../../../../src/helpers'
 import { TestLanguageClient } from '../../../../src/TestLanguageClient'
 import { getInitializedClient } from '../../../client'
@@ -7,27 +8,68 @@ import { getProjectPath, makePosition, makeRange } from '../../../helpers'
 let client!: TestLanguageClient
 
 describe('[foundry] definition', () => {
-  beforeEach(async () => {
+  let testPath: string
+  let importerPath: string
+  let importedPath: string
+  let otherImportedPath: string
+
+  before(async () => {
     client = await getInitializedClient()
+    testPath = getProjectPath('foundry/src/definition/Test.sol')
+    importerPath = getProjectPath('foundry/src/definition/Importer.sol')
+    importedPath = getProjectPath('foundry/lib/myLib/Imported.sol')
+    otherImportedPath = getProjectPath('foundry/lib/myLib/OtherImported.sol')
+
+    await client.openDocument(testPath)
+    await client.openDocument(importerPath)
   })
 
-  afterEach(async () => {
+  after(async () => {
     client.closeAllDocuments()
   })
 
-  describe('[single-file] - go to definition', function () {
-    it('should go to definition', async () => {
-      const documentPath = getProjectPath('foundry/src/definition/Test.sol')
-      const documentUri = toUri(documentPath)
+  test('[single-file] - go to definition', async function () {
+    const location = await client.findDefinition(toUri(testPath), makePosition(14, 25))
 
-      await client.openDocument(documentPath)
+    expect(location).to.deep.equal({
+      uri: testPath,
+      range: makeRange(9, 11, 9, 16),
+    })
+  })
 
-      const location = await client.findDefinition(documentUri, makePosition(14, 25))
+  test('[multi-file][remappings] - go to Imported.sol via import line', async function () {
+    const location = await client.findDefinition(toUri(importerPath), makePosition(4, 22))
 
-      expect(location).to.deep.equal({
-        uri: documentPath,
-        range: makeRange(9, 11, 9, 16),
-      })
+    expect(location).to.deep.equal({
+      uri: importedPath,
+      range: makeRange(2, 0, 9, 0),
+    })
+  })
+
+  test('[multi-file][remappings] - go to OtherImported.sol via import line', async function () {
+    const location = await client.findDefinition(toUri(importerPath), makePosition(5, 22))
+
+    expect(location).to.deep.equal({
+      uri: otherImportedPath,
+      range: makeRange(2, 0, 9, 0),
+    })
+  })
+
+  test('[multi-file][remappings] - go to Imported.sol via declaration line', async function () {
+    const location = await client.findDefinition(toUri(importerPath), makePosition(8, 5))
+
+    expect(location).to.deep.equal({
+      uri: importedPath,
+      range: makeRange(4, 9, 4, 17),
+    })
+  })
+
+  test('[multi-file][remappings] - go to OtherImported.sol via declaration line', async function () {
+    const location = await client.findDefinition(toUri(importerPath), makePosition(9, 5))
+
+    expect(location).to.deep.equal({
+      uri: otherImportedPath,
+      range: makeRange(4, 9, 4, 22),
     })
   })
 })
