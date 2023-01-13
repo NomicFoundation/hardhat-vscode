cfcc0b0ad730c86927d4f89c397ac173d26a372b
John Kane <john@kanej.me>
2023-01-13 16:45:53 +0000

    refactor(client): remove survey popup
    The survey is complete, so this code can be removed.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,6 +1,5 @@
 import { ExtensionContext } from "vscode";
 import { showAnalyticsAllowPopup } from "./popups/showAnalyticsAllowPopup";
-import { showSoliditySurveyPopup } from "./popups/showSoliditySurveyPopup";
 import { warnOnOtherSolidityExtensions } from "./popups/warnOnOtherSolidityExtensions";
 import { indexHardhatProjects } from "./setup/indexHardhatProjects";
 import { setupCommands } from "./setup/setupCommands";
@@ -37,8 +36,7 @@ export async function activate(context: ExtensionContext) {
   //
   // eslint-disable-next-line @typescript-eslint/no-floating-promises
   showAnalyticsAllowPopup(extensionState);
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  showSoliditySurveyPopup(extensionState); // TODO: Remove this after 2023-01-07
+
   // eslint-disable-next-line @typescript-eslint/no-floating-promises
   warnOnOtherSolidityExtensions(extensionState);
 
