47019a2cf6a1aa304db4db06762f1957ebac8798
Armando Andini <armando.andini@gmail.com>
2023-01-20 11:55:46 -0300

    fix: improve forge binary lookup (#366)
    * fix: improve forge binary lookup
    
    * fix: improve forge binary lookup on client

--- client/src/formatter/forgeFormatter.ts
+++ client/src/formatter/forgeFormatter.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as vscode from "vscode";
 import * as cp from "child_process";
+import { runCmd, runningOnWindows } from "../utils/os";
 
 export async function formatDocument(
   document: vscode.TextDocument
@@ -11,13 +13,16 @@ export async function formatDocument(
     lastLine.range.end
   );
 
+  const forgeCommand = await resolveForgeCommand();
+
   const formatted = await new Promise<string>((resolve, reject) => {
     const currentDocument = vscode.window.activeTextEditor?.document.uri;
     const rootPath = currentDocument
       ? vscode.workspace.getWorkspaceFolder(currentDocument)?.uri.fsPath
       : undefined;
+
     const forge = cp.execFile(
-      "forge",
+      forgeCommand,
       ["fmt", "--raw", "-"],
       { cwd: rootPath },
       (err, stdout) => {
@@ -34,4 +39,41 @@ export async function formatDocument(
   });
 
   return [vscode.TextEdit.replace(fullTextRange, formatted)];
+}
+
+async function resolveForgeCommand() {
+  const potentialForgeCommands = ["forge"];
+
+  if (runningOnWindows()) {
+    potentialForgeCommands.push(
+      `${process.env.USERPROFILE}\\.cargo\\bin\\forge`
+    );
+  } else {
+    potentialForgeCommands.push(`${process.env.HOME}/.foundry/bin/forge`);
+  }
+
+  for (const potentialForgeCommand of potentialForgeCommands) {
+    try {
+      await runCmd(`${potentialForgeCommand} --version`);
+      return potentialForgeCommand;
+    } catch (error: any) {
+      if (
+        error.code === 127 || // unix
+        error.toString().includes("is not recognized") || // windows (code: 1)
+        error.toString().includes("cannot find the path") // windows (code: 1)
+      ) {
+        // command not found, then try the next potential command
+        continue;
+      } else {
+        // command found but execution failed
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(
+    `Couldn't find forge binary. Performed lookup: ${JSON.stringify(
+      potentialForgeCommands
+    )}`
+  );
 }
