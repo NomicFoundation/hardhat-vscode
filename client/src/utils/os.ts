bd718bdba980483926136db989270a43fb4a4e28
Armando Andini <armando.andini@gmail.com>
2023-02-15 11:43:59 -0300

    chore: cache resolved forge path on client

--- client/src/utils/os.ts
+++ client/src/utils/os.ts
@@ -1,9 +1,13 @@
-import { exec } from "child_process";
+import { execFile } from "child_process";
 import os from "os";
 
-export async function runCmd(cmd: string, cwd?: string): Promise<string> {
+export async function runCmd(
+  cmd: string,
+  args: string[],
+  cwd?: string
+): Promise<string> {
   return new Promise((resolve, reject) => {
-    exec(cmd, { cwd }, function (error, stdout) {
+    execFile(cmd, args, { cwd }, function (error, stdout) {
       if (error !== null) {
         reject(error);
       }
