a0957f9b80eec025bd47e7d5c73012f8fa54a6ae
Armando Andini <armando.andini@gmail.com>
2023-04-21 11:06:04 -0300

    update hardhat adapter priority

--- server/src/frameworks/Hardhat/HardhatProject.ts
+++ server/src/frameworks/Hardhat/HardhatProject.ts
@@ -47,7 +47,7 @@ export enum WorkerStatus {
 const REQUEST_TIMEOUT = 5000;
 
 export class HardhatProject extends Project {
-  public priority = 3;
+  public priority = 4;
 
   public workerProcess?: ChildProcess;
 
