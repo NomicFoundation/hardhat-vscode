4fe2396d736f8a2e8f5d23ebb0e506077fec5ef8
Armando Andini <armando.andini@gmail.com>
2023-10-30 08:05:35 -0300

    leverage trackTimingSync error handling

--- server/src/services/documentSymbol/onDocumentSymbol.ts
+++ server/src/services/documentSymbol/onDocumentSymbol.ts
@@ -8,8 +8,6 @@ import semver from "semver";
 import _ from "lodash";
 import { Language } from "@nomicfoundation/slang/language";
 import { ProductionKind } from "@nomicfoundation/slang/kinds";
-import { Cursor } from "@nomicfoundation/slang/cursor";
-import { performance } from "perf_hooks";
 import { ServerState } from "../../types";
 import { walk } from "../../parser/slangHelpers";
 import { SymbolTreeBuilder } from "./SymbolTreeBuilder";
@@ -36,7 +34,7 @@ export function onDocumentSymbol(serverState: ServerState) {
   return async (
     params: DocumentSymbolParams
   ): Promise<DocumentSymbol[] | SymbolInformation[] | null> => {
-    const { telemetry, logger, solcVersions } = serverState;
+    const { telemetry, solcVersions } = serverState;
     return telemetry.trackTimingSync("onDocumentSymbol", (transaction) => {
       const { uri } = params.textDocument;
 
@@ -44,11 +42,7 @@ export function onDocumentSymbol(serverState: ServerState) {
       const document = serverState.documents.get(uri);
 
       if (document === undefined) {
-        logger.error("document not found in collection");
-        return {
-          status: "internal_error",
-          result: null,
-        };
+        throw new Error(`${uri} not found in documents`);
       }
 
       const text = document.getText();
@@ -61,105 +55,95 @@ export function onDocumentSymbol(serverState: ServerState) {
         semver.maxSatisfying(solcVersions, versionPragmas.join(" ")) ||
         _.last(solcVersions);
 
-      try {
-        // Parse using slang
-        span = transaction.startChild({ op: "slang-parsing" });
-        const language = new Language(solcVersion!);
+      // Parse using slang
+      span = transaction.startChild({ op: "slang-parsing" });
+      const language = new Language(solcVersion!);
 
-        const parseOutput = language.parse(
-          ProductionKind.SourceUnit,
-          document.getText()
+      const parseOutput = language.parse(
+        ProductionKind.SourceUnit,
+        document.getText()
+      );
+
+      const parseTree = parseOutput.parseTree;
+      span.finish();
+
+      if (parseTree === null) {
+        const strings = parseOutput.errors.map((e: any) =>
+          e.toErrorReport(uri, text, false)
         );
 
-        const parseTree = parseOutput.parseTree;
-        span.finish();
-
-        if (parseTree === null) {
-          logger.error("Slang parsing error");
-          const strings = parseOutput.errors.map((e: any) =>
-            e.toErrorReport(uri, text, false)
-          );
-          logger.error(strings.join(""));
-
-          return {
-            status: "internal_error",
-            result: null,
-          };
-        }
-
-        // let count = 0;
-        // // const start = performance.now();
-        // const kursor: Cursor = parseTree.cursor.clone();
-        // do {
-        //   console.log(
-        //     `${"  ".repeat(kursor.pathRuleNodes.length)}${kursor.node.kind}: ${
-        //       kursor.node?.text
-        //     }`
-        //   );
-        // } while (kursor.goToNext());
-        // const end = performance.now();
-        // const elapsed = end - start;
-        // console.log({ elapsed });
-        // console.log({ count });
-
-        // const myobj = { foo: { bar: 123 } };
-        // const node = kursor.node;
-        // const start = performance.now();
-        // for (let index = 0; index < 1_000_000; index++) {
-        //   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        //   myobj.foo.bar;
-        //   // node.type;
-        // }
-        // const end = performance.now();
-        // const elapsed = end - start;
-        // console.log({ elapsed });
-
-        const builder = new SymbolTreeBuilder();
-
-        const visitors: SymbolVisitor[] = [
-          new StructDefinition(document, builder),
-          new StructMember(document, builder),
-          new InterfaceDefinition(document, builder),
-          new FunctionDefinition(document, builder),
-          new ContractDefinition(document, builder),
-          new EventDefinition(document, builder),
-          new StateVariableDeclaration(document, builder),
-          new VariableDeclaration(document, builder),
-          new ConstantDefinition(document, builder),
-          new ConstructorDefinition(document, builder),
-          new EnumDefinition(document, builder),
-          new ErrorDefinition(document, builder),
-          new FallbackFunctionDefinition(document, builder),
-          new LibraryDefinition(document, builder),
-          new ModifierDefinition(document, builder),
-          new ReceiveFunctionDefinition(document, builder),
-          new UserDefinedValueTypeDefinition(document, builder),
-        ];
-
-        // Walk the tree and generate symbol nodes
-        span = transaction.startChild({ op: "walk-generate-symbols" });
-        walk(
-          parseTree.cursor,
-          (nodeWrapper) => {
-            for (const visitor of visitors) {
-              visitor.enter(nodeWrapper);
-            }
-          },
-          (nodeWrapper) => {
-            for (const visitor of visitors) {
-              visitor.exit(nodeWrapper);
-            }
-          }
-        );
-        span.finish();
-        // console.log("ALL GOOD");
-        // console.log(JSON.stringify(builder.symbols, null, 2));
-
-        return { status: "ok", result: builder.symbols };
-      } catch (error) {
-        logger.error(error);
-        return { status: "internal_error", result: null };
+        throw new Error(`Slang parsing error: ${strings.join("")}`);
       }
+
+      // let count = 0;
+      // // const start = performance.now();
+      // const kursor: Cursor = parseTree.cursor.clone();
+      // do {
+      //   console.log(
+      //     `${"  ".repeat(kursor.pathRuleNodes.length)}${kursor.node.kind}: ${
+      //       kursor.node?.text
+      //     }`
+      //   );
+      // } while (kursor.goToNext());
+      // const end = performance.now();
+      // const elapsed = end - start;
+      // console.log({ elapsed });
+      // console.log({ count });
+
+      // const myobj = { foo: { bar: 123 } };
+      // const node = kursor.node;
+      // const start = performance.now();
+      // for (let index = 0; index < 1_000_000; index++) {
+      //   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      //   myobj.foo.bar;
+      //   // node.type;
+      // }
+      // const end = performance.now();
+      // const elapsed = end - start;
+      // console.log({ elapsed });
+
+      const builder = new SymbolTreeBuilder();
+
+      const visitors: SymbolVisitor[] = [
+        new StructDefinition(document, builder),
+        new StructMember(document, builder),
+        new InterfaceDefinition(document, builder),
+        new FunctionDefinition(document, builder),
+        new ContractDefinition(document, builder),
+        new EventDefinition(document, builder),
+        new StateVariableDeclaration(document, builder),
+        new VariableDeclaration(document, builder),
+        new ConstantDefinition(document, builder),
+        new ConstructorDefinition(document, builder),
+        new EnumDefinition(document, builder),
+        new ErrorDefinition(document, builder),
+        new FallbackFunctionDefinition(document, builder),
+        new LibraryDefinition(document, builder),
+        new ModifierDefinition(document, builder),
+        new ReceiveFunctionDefinition(document, builder),
+        new UserDefinedValueTypeDefinition(document, builder),
+      ];
+
+      // Walk the tree and generate symbol nodes
+      span = transaction.startChild({ op: "walk-generate-symbols" });
+      walk(
+        parseTree.cursor,
+        (nodeWrapper) => {
+          for (const visitor of visitors) {
+            visitor.enter(nodeWrapper);
+          }
+        },
+        (nodeWrapper) => {
+          for (const visitor of visitors) {
+            visitor.exit(nodeWrapper);
+          }
+        }
+      );
+      span.finish();
+      // console.log("ALL GOOD");
+      // console.log(JSON.stringify(builder.symbols, null, 2));
+
+      return { status: "ok", result: builder.symbols };
     });
   };
 }
