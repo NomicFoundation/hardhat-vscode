63663a57955f9ffb1ce045ce28eee0e94e580e79
Igor Matuszewski <xanewok@gmail.com>
2024-02-21 19:52:26 +0100

    refactor: Support more deeply nested node symbol names in SymbolVisitor

--- server/src/services/documentSymbol/visitors/StructDefinition.ts
+++ server/src/services/documentSymbol/visitors/StructDefinition.ts
@@ -1,9 +1,9 @@
 import { SymbolKind } from "vscode-languageserver-types";
-import { RuleKind, TokenKind } from "@nomicfoundation/slang/kinds";
+import { FieldName, RuleKind, TokenKind } from "@nomicfoundation/slang/kinds";
 import { SymbolVisitor } from "../SymbolVisitor";
 
 export class StructDefinition extends SymbolVisitor {
   public ruleKind = RuleKind.StructDefinition;
   public symbolKind = SymbolKind.Struct;
-  public nameTokenKind = TokenKind.Identifier;
+  public nameToken = [FieldName.Name, TokenKind.Identifier] as const;
 }
