80624fcb56e86ee3b1238985e4fd2a9dbf9726d0
John Kane <john@kanej.me>
2025-01-29 17:26:40 +0000

    chore: update eslint errors
    New rules are affecting our naming conventions. Rather than disable the
    rule I have scattered disable lines. This is a tradeoff, but we expect a
    revamp of the eslint rules based on HH3 in the near future.

--- server/src/services/completion/defaultCompletion.ts
+++ server/src/services/completion/defaultCompletion.ts
@@ -196,6 +196,7 @@ export const globalVariables: GlobalVariablesType = {
     "number",
     "timestamp",
   ],
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   "msg.sender": [
     "balance",
     "code",
