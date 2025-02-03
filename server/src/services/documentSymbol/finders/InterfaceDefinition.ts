fdfd6470008cad8e60c4be35ea96cf278ebcad84
Armando Andini <armando.andini@gmail.com>
2025-02-03 14:56:30 -0300

    Upgrade to slang 0.19

--- server/src/services/documentSymbol/finders/InterfaceDefinition.ts
+++ server/src/services/documentSymbol/finders/InterfaceDefinition.ts
@@ -1,13 +1,16 @@
 import { SymbolKind } from "vscode-languageserver-types";
-import { Query } from "@nomicfoundation/slang/query";
+import type { Query } from "@nomicfoundation/slang/cst" with { "resolution-mode": "import" };
 import { SymbolFinder } from "../SymbolFinder";
 
 export class InterfaceDefinition extends SymbolFinder {
   public override readonly symbolKind = SymbolKind.Interface;
 
-  public override readonly query = Query.parse(`
-    @definition [InterfaceDefinition
-      @identifier name: [_]
-    ]
-  `);
+  public override async getQuery(): Promise<Query> {
+    const { Query } = await import("@nomicfoundation/slang/cst");
+    return Query.create(`
+      @definition [InterfaceDefinition
+        @identifier name: [_]
+      ]
+    `);
+  }
 }
