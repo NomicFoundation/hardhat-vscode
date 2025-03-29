e3f28a0ba1d244bcf8b80e8ee338158e7bd00461
John Kane <john@kanej.me>
2025-03-29 14:20:32 +0000

    test: add tests to cover the send behaviour
    These tests confirm that the promise behaviour matches expectations.
    They do not cover all the methods of HardhatProject that are effected by
    this change.

--- server/test/frameworks/hardhat/HardhatProject.test.ts
+++ server/test/frameworks/hardhat/HardhatProject.test.ts
@@ -5,8 +5,15 @@ import {
   HardhatProject,
   WorkerStatus,
 } from "../../../src/frameworks/Hardhat/HardhatProject";
-import { FileBelongsRequest } from "../../../src/frameworks/Hardhat/worker/WorkerProtocol";
+import {
+  BuildCompilationRequest,
+  FileBelongsRequest,
+  RequestMessage,
+} from "../../../src/frameworks/Hardhat/worker/WorkerProtocol";
 import { ServerState } from "../../../src/types";
+import { CompilationDetails } from "../../../src/frameworks/base/CompilationDetails";
+
+import { InitializationFailedError } from "../../../src/frameworks/base/Errors";
 
 describe("HardhatProject", function () {
   let project: HardhatProject;
@@ -24,6 +31,147 @@ describe("HardhatProject", function () {
 
     project.workerProcess = workerProcessMock;
     project.workerStatus = WorkerStatus.RUNNING;
+  });
+
+  describe.only("buildCompilation", function () {
+    let sendRequest: BuildCompilationRequest | null = null;
+    const fakedCompilationResponse: CompilationDetails = {
+      solcVersion: "0.8.20",
+      input: {
+        language: "solidity",
+        sources: {},
+        settings: { optimizer: {}, outputSelection: {} },
+      },
+    };
+
+    beforeEach(async () => {
+      sendRequest = null;
+
+      workerProcessMock.send = function (request: RequestMessage) {
+        sendRequest = request as BuildCompilationRequest;
+
+        (project as any)._handleResponse(
+          request.requestId,
+          fakedCompilationResponse
+        );
+
+        return true;
+      };
+    });
+
+    describe("when initialization was correct and the worker is operative", function () {
+      it("sends a BuildCompilationRequest to the worker", async () => {
+        await project.buildCompilation("./contracts/examples.sol", []);
+
+        if (sendRequest === null) {
+          assert.fail("Sent request should be defined");
+        }
+
+        const buildCompilationRequest = sendRequest as BuildCompilationRequest;
+
+        assert.equal(
+          buildCompilationRequest.sourceUri,
+          "./contracts/examples.sol"
+        );
+      });
+    });
+
+    describe("when initialization failed", function () {
+      it("throws an error indicating init failed", async () => {
+        project.workerStatus = WorkerStatus.ERRORED;
+        project.workerLoadFailureReason = "Problem loading Hardhat";
+
+        let caughtException = false;
+        try {
+          await project.buildCompilation("./contracts/examples.sol", []);
+
+          assert.fail(
+            "The build compilation should be blocked because the underlying worker never started"
+          );
+        } catch (error) {
+          caughtException = true;
+
+          if (error === undefined) {
+            assert.fail(
+              "Expected an Error to be thrown on an uninitialized error"
+            );
+          }
+
+          const uninitStruct = error as InitializationFailedError;
+
+          assert.ok(uninitStruct._isInitializationFailedError);
+          assert.equal(uninitStruct.error, "Problem loading Hardhat");
+        }
+
+        assert.ok(caughtException);
+      });
+    });
+
+    describe("when initialization was correct but the worker is now stopped", function () {
+      it("throws an error indicating the worker is not running", async () => {
+        project.workerStatus = WorkerStatus.STOPPED;
+
+        let caughtException = false;
+        try {
+          await project.buildCompilation("./contracts/examples.sol", []);
+
+          assert.fail(
+            "The build compilation should be blocked because the underlying worker is blocked"
+          );
+        } catch (error) {
+          caughtException = true;
+          if (!(error instanceof Error)) {
+            assert.fail(
+              "Expected an Error to be thrown on a stopped Worker process"
+            );
+          }
+          assert.ok(error.message.includes("Worker is not running."));
+        }
+
+        assert.ok(caughtException);
+      });
+    });
+
+    describe("when initialization was correct but the worker has disconnected (i.e. danger of EPIPE)", function () {
+      it("throws an error indicating the worker is not connected", async () => {
+        project.workerStatus = WorkerStatus.RUNNING;
+
+        if (project.workerProcess === undefined) {
+          return assert.fail("Worker process should be defined");
+        }
+
+        // Set `project.workerProcess.connected = false`
+        Object.defineProperty(project.workerProcess, "connected", {
+          value: false,
+          writable: true,
+        });
+
+        let caughtException = false;
+        try {
+          await project.buildCompilation("./contracts/examples.sol", []);
+
+          assert.fail(
+            "The build compilation should be blocked because the underlying worker is blocked"
+          );
+        } catch (error) {
+          caughtException = true;
+
+          if (!(error instanceof Error)) {
+            assert.fail(
+              "Expected an Error to be thrown on a stopped Worker process"
+            );
+          }
+
+          assert.ok(
+            error.message.includes(
+              "Hardhat Worker process is not connected - cannot send message."
+            )
+          );
+        }
+
+        assert.ok(caughtException);
+      });
+    });
   });
 
   describe("fileBelongs", function () {
