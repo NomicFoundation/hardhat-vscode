702ca0ea74463f80d0613d47a517fbb4a9b1d1bf
Armando Andini <armando.andini@gmail.com>
2025-04-08 12:25:50 -0300

    Sentry client side setup

--- server/src/services/documentSymbol/onDocumentSymbol.ts
+++ server/src/services/documentSymbol/onDocumentSymbol.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
