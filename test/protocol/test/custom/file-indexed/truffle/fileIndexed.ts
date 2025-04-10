85aee95d58a1cdb01cd98a11d91580696c2882cb
Armando Andini <armando.andini@gmail.com>
2025-04-10 16:28:06 -0300

    Add protocol tests for custom/file-indexed (project associated to files)

---
+++ test/protocol/test/custom/file-indexed/truffle/fileIndexed.ts
@@ -0,0 +1,23 @@
+import { test } from 'mocha'
+import { TestLanguageClient } from '../../../../src/TestLanguageClient'
+import { getInitializedClient } from '../../../client'
+import { getProjectPath } from '../../../helpers'
+
+let client!: TestLanguageClient
+
+describe('[truffle] custom/file-indexed', () => {
+  before(async () => {
+    client = await getInitializedClient()
+  })
+
+  test('after initialization, a notification is received with the indexed file uri and project', async () => {
+    const contractPath = getProjectPath('truffle/my_contracts/codeAction/NoLicense.sol')
+
+    const expectedData = {
+      uri: contractPath,
+      project: { frameworkName: 'Truffle', configPath: getProjectPath('truffle/truffle-config.js') },
+    }
+
+    await client.getOrWaitNotification('custom/file-indexed', expectedData)
+  })
+})
